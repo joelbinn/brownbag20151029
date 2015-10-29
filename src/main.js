8e34d7d76af7a0fab55b1dd27d051df075925660
Joel Binnquist <joel.binnquist@tillvaxtverket.se>
2015-10-29 13:18:24 +0100

    Introduced manual bootstrapping of Angular.

--- src/main.js
+++ src/main.js
@@ -5,6 +5,9 @@ import angular from 'angular';
 import {chucknorris} from './chucknorris/module';
 
 const mainModule = angular.module('main',[chucknorris.name]);
+// Use manual boostrapping of angular since something went fishy
+// with order of loading.
+angular.bootstrap(document, [mainModule.name]);
 
 const fn = ()=>'Yowza brownbaggins!'
 console.debug(fn());
